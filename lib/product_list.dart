544dbb43bb5378d49d3d956a70023066eeda5a5f
sukumar <sukumar.mennan@gmail.com>
2020-10-09 21:47:01 +0530

    [Sukumar] Add dropdown

--- lib/product_list.dart
+++ lib/product_list.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:html';
 
 import 'package:flutter/material.dart';
 import 'package:starter_flutter/product.dart';
@@ -12,39 +13,91 @@ class ProductList extends StatefulWidget {
 }
 
 class _ProductListState extends State<ProductList> {
-  Future<List<Product>> getItems() async {
-    var response = await http
-        .get('https://run.mocky.io/v3/86714065-50f3-423b-a048-ee58c46733ea');
+  String _sortValue = "A-Z";
+
+  Future<List<Product>> _getItems() async {
+    String url = 'https://run.mocky.io/v3/86714065-50f3-423b-a048-ee58c46733ea';
+    switch (_sortValue) {
+      case "Z-A":
+        url = 'https://run.mocky.io/v3/86714065-50f3-423b-a048-ee58c46733ea';
+        break;
+      case "Price":
+        url = 'https://run.mocky.io/v3/86714065-50f3-423b-a048-ee58c46733ea';
+        break;
+      default:
+        break;
+    }
+    var response = await http.get(url);
     var jsonBody = jsonDecode(response.body);
     return productsFromJson(jsonBody);
   }
+
+  onSortPressed() => (value) {
+        this.setState(() {
+          _sortValue = value;
+        });
+      };
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       drawer: ProductDrawer(),
       appBar: AppBar(
-        title: Text('Flutter Workshop'),
+        title: SortDropDown(
+          onSortPressed: onSortPressed(),
+        ),
       ),
       body: FutureBuilder(
-          future: getItems(),
-          builder: (context, snapshot) {
-            switch (snapshot.connectionState) {
-              case ConnectionState.none:
-                return Text("None");
-              case ConnectionState.waiting:
-                return Center(
-                  child: CircularProgressIndicator(),
-                );
-              case ConnectionState.active:
-                return Text("Success");
-              case ConnectionState.done:
-                return ProductListItems(products: snapshot.data);
-              default:
-                return Text("Uknown failure");
-                break;
-            }
-          }),
+        future: _getItems(),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              return Text("None");
+            case ConnectionState.waiting:
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            case ConnectionState.active:
+              return Text("Success");
+            case ConnectionState.done:
+              return ProductListItems(products: snapshot.data);
+            default:
+              return Text("Unknown failure");
+              break;
+          }
+        },
+      ),
     );
+  }
+}
+
+class SortDropDown extends StatefulWidget {
+  const SortDropDown({
+    @required FunctionStringCallback onSortPressed,
+  }) : _onSortPressed = onSortPressed;
+
+  final FunctionStringCallback _onSortPressed;
+
+  @override
+  _SortDropDownState createState() => _SortDropDownState();
+}
+
+class _SortDropDownState extends State<SortDropDown> {
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton(items: [
+      DropdownMenuItem(
+        child: Text("A-Z"),
+        value: "A-Z",
+      ),
+      DropdownMenuItem(
+        child: Text("Z-A"),
+        value: "Z-A",
+      ),
+      DropdownMenuItem(
+        child: Text("Price"),
+        value: "Price",
+      )
+    ], onChanged: widget._onSortPressed);
   }
 }
