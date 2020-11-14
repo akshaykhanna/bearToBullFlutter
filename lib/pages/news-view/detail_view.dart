382127aa23ae0b9b8aa72eb87e1cb073217ceab6
Akshay Khanna <akshaykhanna@Akshays-MacBook-Pro.local>
2020-11-14 11:48:27 +0530

    Add newsDetails detail props

--- lib/pages/news-view/detail_view.dart
+++ lib/pages/news-view/detail_view.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:redux/redux.dart';
+import 'package:starter_flutter/actions/news_actions.dart';
+import 'package:starter_flutter/models/app_state.dart';
+import 'package:starter_flutter/models/news_state.dart';
 import 'package:starter_flutter/pages/news-view/news_card.dart';
 // import 'package:starter_flutter/pages/news-view/swiper_view.dart';
 
@@ -18,4 +22,24 @@ class DetailView extends StatelessWidget {
         body: NewsCard(args: args));
     // body: SwipeCards(args.id));
   }
+}
+
+class NewsListScreenProps {
+  final Function getNewsDetailList;
+  // final Function getUserDetails;
+  final NewsDetailsState detailResponse;
+
+  NewsListScreenProps({
+    this.getNewsDetailList,
+    this.detailResponse,
+    // this.getUserDetails,
+  });
+}
+
+NewsListScreenProps mapStateToProps(Store<AppState> store) {
+  return NewsListScreenProps(
+    detailResponse: store.state.news.details,
+    getNewsDetailList: (int id) => store.dispatch(getNewsDetails(id)),
+    // getUserDetails: (int id) => store.dispatch(getUserDetails(id)),
+  );
 }
